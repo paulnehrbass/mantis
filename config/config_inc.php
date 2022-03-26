2198a0200cddf6e1c9cd760052071ba35fa8f1f3
paul.nehrbass <paul.nehrbass@web.de>
2022-03-26 11:53:40 +0100

    0000001: Initiale Mantis Installation http://localhost/mantis/view.php?id=1

---
+++ config/config_inc.php
@@ -0,0 +1,13 @@
+<?php
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'bugtracker';
+$g_db_username            = 'root';
+$g_db_password            = '';
+
+$g_default_timezone       = 'Europe/Berlin';
+
+$g_crypto_master_salt     = 'lx3X4qSKLS4sQIndP9n+h4e5/RWMCjYSOP59fLa5wT0=';
+
+# --- Security ---
+$g_crypto_master_salt = 'dsfsdfjhkadkshlfaguirefhjgsngrtgruhgaörhgaergh';	#  Random string of at least 16 chars, unique to the installation
